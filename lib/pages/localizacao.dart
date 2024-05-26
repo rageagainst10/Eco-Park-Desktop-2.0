00697cfdfbe6c6cb68a3a18a01b10893f4568689
Luis Felipe <90076273+rageagainst10@users.noreply.github.com>
2024-05-26 20:15:30 -0300

    conrado viado

---
+++ lib/pages/localizacao.dart
@@ -0,0 +1,192 @@
+import 'package:ecoparkdesktop/pages/gerenciamentoDeReservas.dart';
+import 'package:ecoparkdesktop/pages/login.dart';
+import 'package:ecoparkdesktop/widgets/CaixaDeTextoCadastro.dart';
+import 'package:flutter/material.dart';
+
+// Classe modelo para representar os dados do formulário
+class FormularioData {
+  final String tempoLR;
+  final String taxaReserva;
+  final String nomeLocalizacao;
+  final String endereco;
+  final String taxaCancelamento;
+  final String taxaCustoPorHora;
+
+  FormularioData({
+    required this.tempoLR,
+    required this.taxaReserva,
+    required this.nomeLocalizacao,
+    required this.endereco,
+    required this.taxaCancelamento,
+    required this.taxaCustoPorHora,
+  });
+}
+
+class Cadastro extends StatefulWidget {
+  const Cadastro({Key? key}) : super(key: key);
+
+  @override
+  State<Cadastro> createState() => _CadastroState();
+}
+
+class _CadastroState extends State<Cadastro> {
+  final TextEditingController _tempoLRController = TextEditingController();
+  final TextEditingController _taxaReservaController = TextEditingController();
+  final TextEditingController _nomeLocalizacaoController = TextEditingController();
+  final TextEditingController _enderecoController = TextEditingController();
+  final TextEditingController _taxaCancelamentoController = TextEditingController();
+  final TextEditingController _taxaCustoPorHoraController = TextEditingController();
+
+  void _enviarDadosParaAPI() {
+    // Criar uma instância do modelo de dados com os valores dos campos de texto
+    FormularioData data = FormularioData(
+      tempoLR: _tempoLRController.text,
+      taxaReserva: _taxaReservaController.text,
+      nomeLocalizacao: _nomeLocalizacaoController.text,
+      endereco: _enderecoController.text,
+      taxaCancelamento: _taxaCancelamentoController.text,
+      taxaCustoPorHora: _taxaCustoPorHoraController.text,
+    );
+
+    // Lógica para enviar os dados para a API
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Container(
+          width: 350,
+          height: 500,
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: const Color(0xFF8DCBC8),
+              width: 2.0,
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const SizedBox(height: 10),
+              const Text(
+                "Localização",
+                style: TextStyle(
+                  color: Color(0xFF8DCBC8),
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Tempo LR',
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.all(8.0),
+                        ),
+                        controller: _tempoLRController,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 150,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 1.0,
+                      ),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Center(
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Taxa de reserva',
+                          border: InputBorder.none,
+                          contentPadding: EdgeInsets.all(8.0),
+                        ),
+                        controller: _taxaReservaController,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
+              CaixaDeTextoCadastro(
+                texto: 'Nome localização',
+                controller: _nomeLocalizacaoController,
+              ),
+              const SizedBox(height: 15),
+              CaixaDeTextoCadastro(
+                texto: 'Endereço',
+                controller: _enderecoController,
+              ),
+              const SizedBox(height: 15),
+              CaixaDeTextoCadastro(
+                texto: 'Taxa de cancelamento',
+                controller: _taxaCancelamentoController,
+              ),
+              const SizedBox(height: 15),
+              CaixaDeTextoCadastro(
+                texto: 'Taxa custo por hora',
+                controller: _taxaCustoPorHoraController,
+              ),
+              const SizedBox(height: 15),
+              Container(
+                height: 40,
+                width: 315,
+                child: TextButton(
+                  onPressed: () {
+                    _enviarDadosParaAPI();
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => GerenciamentoDeReserva()),
+                    );
+                  }, 
+                  style: ButtonStyle(
+                    side: MaterialStateProperty.all<BorderSide>(
+                      const BorderSide(
+                        color: Color(0xFF8DCBC8),
+                        width: 2.0,
+                      ),
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'Cadastrar',
+                    style: TextStyle(
+                      color: Color(0xFF8DCBC8),
+                    ),
+                  ),
+                ),
+              ),
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: Cadastro(),
+  ));
+}
