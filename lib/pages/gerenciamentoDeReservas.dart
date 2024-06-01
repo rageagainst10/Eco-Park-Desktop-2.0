04d70dd46adbb26b117912c8db040a2a0b37ca71
Luis Felipe <90076273+rageagainst10@users.noreply.github.com>
2024-05-31 22:13:55 -0300

    tela de gerenciamento de reserva atualizada

--- lib/pages/gerenciamentoDeReservas.dart
+++ lib/pages/gerenciamentoDeReservas.dart
@@ -15,12 +15,14 @@ import 'cadastroFuncionario.dart';
 
 class GerenciamentoDeReserva extends StatefulWidget {
   const GerenciamentoDeReserva({Key? key}) : super(key: key);
+
   @override
   State<GerenciamentoDeReserva> createState() => _GerenciamentoDeReservaState();
 }
 
 class _GerenciamentoDeReservaState extends State<GerenciamentoDeReserva> {
   final StorageService _storageService = getIt<StorageService>();
+  final TextEditingController _textController = TextEditingController();
 
   List<LocationModel> _estabelecimentos = [];
   LocationModel? _estabelecimentoSelecionado;
@@ -28,44 +30,16 @@ class _GerenciamentoDeReservaState extends State<GerenciamentoDeReserva> {
   List<ParkingSpaceModel> _vagasEditadas = [];
   bool _isLoading = true;
 
-  void _onVagaEditada(ParkingSpaceModel vagaEditada) {
-    setState(() {
-      final index = _vagas.indexWhere((vaga) => vaga.id == vagaEditada.id);
-      if (index != -1) {
-        _vagas[index] = vagaEditada; // Substitui a vaga antiga pela editada
-      }
-      _vagasEditadas.add(
-          vagaEditada); // Adiciona à lista de vagas editadas para salvar na API
-    });
-  }
-
-  void _saveChanges() async {
-    try {
-      for (var vaga in _vagasEditadas) {
-        await ReservaRepository(_storageService).salvarAlteracoes(vaga);
-      }
-
-      // Limpar a lista de vagas editadas após salvar
-      setState(() {
-        _vagasEditadas = [];
-      });
-
-      // Exibir mensagem de sucesso
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Alterações salvas com sucesso!')),
-      );
-    } catch (e) {
-      // Tratar erro ao salvar
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Erro ao salvar alterações: $e')),
-      );
-    }
-  }
-
   @override
   void initState() {
     super.initState();
     _loadEstabelecimentos();
+  }
+
+  @override
+  void dispose() {
+    _textController.dispose();
+    super.dispose();
   }
 
   Future<void> _loadEstabelecimentos() async {
@@ -77,32 +51,7 @@ class _GerenciamentoDeReservaState extends State<GerenciamentoDeReserva> {
         _isLoading = false;
       });
     } catch (e) {
-      // Tratar erro
-    }
-  }
-
-  int compareParkingSpaces(ParkingSpaceModel a, ParkingSpaceModel b) {
-    final RegExp regex =
-        RegExp(r'(\d+)'); // Expressão regular para extrair números
-
-    final matchA = regex.firstMatch(a.name);
-    final matchB = regex.firstMatch(b.name);
-
-    if (matchA == null || matchB == null) {
-      return a.name.compareTo(
-          b.name); // Se não houver números, compara as strings normalmente
-    }
-
-    final numA =
-        int.parse(matchA.group(0)!); // Extrai e converte o número para inteiro
-    final numB = int.parse(matchB.group(0)!);
-
-    if (numA == numB) {
-      return a.name
-          .compareTo(b.name); // Se os números forem iguais, compara as strings
-    } else {
-      return numA.compareTo(
-          numB); // Se os números forem diferentes, compara os números
+      // Handle error
     }
   }
 
@@ -112,166 +61,158 @@ class _GerenciamentoDeReservaState extends State<GerenciamentoDeReserva> {
     });
     try {
       final locations = await ReservaRepository(_storageService).getLocations();
-
-      // Encontra a localização com o ID especificado
       final location =
           locations.firstWhere((loc) => loc.id == estabelecimentoId);
-
-      // Ordena as vagas da localização encontrada
       location.parkingSpaces.sort(compareParkingSpaces);
       setState(() {
-        _vagas = location
-            .parkingSpaces; // Supondo que só há uma localização por estabelecimento
+        _vagas = location.parkingSpaces;
         _isLoading = false;
       });
     } catch (e) {
-      // Tratar erro
+      // Handle error
     }
   }
 
-  TextEditingController _textController = TextEditingController();
+  int compareParkingSpaces(ParkingSpaceModel a, ParkingSpaceModel b) {
+    final RegExp regex = RegExp(r'(\d+)');
+    final matchA = regex.firstMatch(a.name);
+    final matchB = regex.firstMatch(b.name);
 
-  @override
-  void dispose() {
-    _textController.dispose();
-    super.dispose();
+    if (matchA == null || matchB == null) {
+      return a.name.compareTo(b.name);
+    }
+
+    final numA = int.parse(matchA.group(0)!);
+    final numB = int.parse(matchB.group(0)!);
+
+    return numA == numB ? a.name.compareTo(b.name) : numA.compareTo(numB);
+  }
+
+  void _onVagaEditada(ParkingSpaceModel vagaEditada) {
+    setState(() {
+      final index = _vagas.indexWhere((vaga) => vaga.id == vagaEditada.id);
+      if (index != -1) {
+        _vagas[index] = vagaEditada;
+      }
+      _vagasEditadas.add(vagaEditada);
+    });
+  }
+
+  void _saveChanges() async {
+    try {
+      for (var vaga in _vagasEditadas) {
+        await ReservaRepository(_storageService).salvarAlteracoes(vaga);
+      }
+      setState(() {
+        _vagasEditadas = [];
+      });
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Alterações salvas com sucesso!')),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Erro ao salvar alterações: $e')),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    _isLoading
-        ? const Center(child: CircularProgressIndicator())
-        : Expanded(
-            child: ListaDeVagas(
-                parkingSpaces: _vagas, onVagaEditada: _onVagaEditada));
     return Scaffold(
       appBar: AppBarPersonalizado(
-        text:
-            'Gerenciamento de reservas', // Passando o texto desejado para o AppBarPersonalizado
+        text: 'Gerenciamento de reservas',
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Color(0xFF8DCBC8),
-              ),
-              child: Text(
-                'Outras Páginas',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
-              ),
-            ),
-            ListTile(
-              title: Text('Gerenciamento de Premios'),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context) => GerenciamentoDePremios()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Cadastro de Localizacao'),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context) => CadastroDeLocalizacao()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Cadastro de Funcionario'),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context) => CadastroDeFuncionario()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Atribuir Permissão'),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => AtribuirPermissao()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Atualizar Dados'),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => AtualizarDados()),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Sair'),
-              onTap: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => Login()),
-                );
-              },
-            ),
-          ],
-        ),
-      ),
+      drawer: _buildDrawer(context),
       body: Center(
-        child: Container(
-          width: 500,
-          height: 500,
-          decoration: BoxDecoration(
-            border: Border.all(
-              color: const Color(0xFF8DCBC8),
-              width: 2.0,
+        child: _isLoading ? const CircularProgressIndicator() : _buildContent(),
+      ),
+    );
+  }
+
+  Widget _buildDrawer(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          const DrawerHeader(
+            decoration: BoxDecoration(color: Color(0xFF8DCBC8)),
+            child: Text(
+              'Outras Páginas',
+              style: TextStyle(color: Colors.white, fontSize: 24),
             ),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              DropdownButton<LocationModel>(
-                value: _estabelecimentoSelecionado,
-                hint: const Text("Selecionar localização"),
-                items: _estabelecimentos.map((location) {
-                  return DropdownMenuItem<LocationModel>(
-                    value: location,
-                    child: Text(location.name),
-                  );
-                }).toList(),
-                onChanged: (LocationModel? newValue) {
-                  setState(() {
-                    _estabelecimentoSelecionado = newValue;
-                    if (newValue != null) {
-                      _loadVagas(newValue.id);
-                    }
-                  });
-                },
-              ),
-              _isLoading
-                  ? const CircularProgressIndicator()
-                  : Expanded(
-                      child: Column(
-                        children: [
-                          Expanded(
-                              child: ListaDeVagas(
-                            parkingSpaces: _vagas,
-                            onVagaEditada: _onVagaEditada,
-                          )), // Grade de vagas
-                          _buildLegenda(), // Legenda de cores
-                          ElevatedButton(
-                            onPressed:
-                                _saveChanges, // Chama o método para salvar as alterações
-                            child: const Text('Salvar Alterações'),
-                          ),
-                        ],
-                      ),
-                    ),
-            ],
+          _buildDrawerItem(
+              context, 'Gerenciamento de Premios', GerenciamentoDePremios()),
+          _buildDrawerItem(
+              context, 'Cadastro de Localizacao', CadastroDeLocalizacao()),
+          _buildDrawerItem(
+              context, 'Cadastro de Funcionario', CadastroDeFuncionario()),
+          _buildDrawerItem(context, 'Atribuir Permissão', AtribuirPermissao()),
+          _buildDrawerItem(context, 'Atualizar Dados', AtualizarDados()),
+          _buildDrawerItem(context, 'Sair', Login()),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildDrawerItem(BuildContext context, String title, Widget page) {
+    return ListTile(
+      title: Text(title),
+      onTap: () {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => page));
+      },
+    );
+  }
+
+  Widget _buildContent() {
+    return Container(
+      width: 500,
+      height: 500,
+      decoration: BoxDecoration(
+        border: Border.all(color: Color(0xFF8DCBC8), width: 1.0),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          DropdownButton<LocationModel>(
+            value: _estabelecimentoSelecionado,
+            hint: const Text("Selecionar localização"),
+            items: _estabelecimentos.map((location) {
+              return DropdownMenuItem<LocationModel>(
+                value: location,
+                child: Text(location.name),
+              );
+            }).toList(),
+            onChanged: (LocationModel? newValue) {
+              setState(() {
+                _estabelecimentoSelecionado = newValue;
+                if (newValue != null) {
+                  _loadVagas(newValue.id);
+                }
+              });
+            },
           ),
-        ),
+          Expanded(
+            child: Column(
+              children: [
+                Expanded(
+                  child: ListaDeVagas(
+                    parkingSpaces: _vagas,
+                    onVagaEditada: _onVagaEditada,
+                  ),
+                ),
+                _buildLegenda(),
+                SizedBox(height: 16), // Espaçamento entre a legenda e o botão
+                _buildSaveButton(),
+                SizedBox(
+                    height:
+                        16), // Espaçamento entre o botão e a parte de baixo do container
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -296,14 +237,35 @@ class _GerenciamentoDeReservaState extends State<GerenciamentoDeReserva> {
         Container(
           width: 16,
           height: 16,
-          decoration: BoxDecoration(
-            color: color,
-            shape: BoxShape.circle,
-          ),
+          decoration: BoxDecoration(color: color, shape: BoxShape.circle),
         ),
         const SizedBox(width: 8),
         Text(label),
       ],
+    );
+  }
+
+  Widget _buildSaveButton() {
+    return Container(
+      height: 40,
+      width: 315,
+      child: TextButton(
+        onPressed: _saveChanges,
+        child: const Text(
+          'Salvar Alterações',
+          style: TextStyle(color: Color(0xFF8DCBC8)),
+        ),
+        style: ButtonStyle(
+          side: MaterialStateProperty.all<BorderSide>(
+            const BorderSide(color: Color(0xFF8DCBC8), width: 2.0),
+          ),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
